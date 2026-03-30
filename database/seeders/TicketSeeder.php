30852adfa3cd215e844eaefca3ee72c0426fb674
tyler36 <7234392+tyler36@users.noreply.github.com>
2026-03-30 17:51:06 +0900

    seed some "real" tickets

--- database/seeders/TicketSeeder.php
+++ database/seeders/TicketSeeder.php
@@ -11,6 +11,22 @@ class TicketSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\Ticket::create([
+            'subject' => 'Database error when creating AI run',
+            'body' => 'errno: 150 "Foreign key constraint is incorrectly formed"',
+        ]);
+
+        \App\Models\Ticket::create([
+            'subject' => 'Bluetooth headset stopped',
+            'body' => 'Audio drops when I press pause',
+        ]);
+
+
+        \App\Models\Ticket::create([
+            'subject' => 'BSOD',
+            'body' => 'After updating Windows 98, I get a blue screen of death every time I try to open Internet Explorer',
+        ]);
+
         \App\Models\Ticket::factory(10)->create();
     }
 }
